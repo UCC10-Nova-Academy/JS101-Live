d1cd5c3df9dfe4f10e5e6a83e3a2c59568bafcb0
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-04-16 20:19:44 +0300

    Some changes

---
+++ P02_JS-Samples/sample01.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function fonksiyonum() {
+    document.getElementById("demo").innerHTML = "Burasý JS ile butona basýldýktan sonra deðistirilmistir....";
+}
+
+function tarih() {
+    document.write("Günün tarihi : " + Date());
+}
