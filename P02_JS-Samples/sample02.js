d7f2906b190b58f4dbf3a2b3806706c42f737c15
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-04-16 20:46:53 +0300

    Some changes

---
+++ P02_JS-Samples/sample02.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function kareAl() {
+    var sayi = parseInt(document.getElementById("sayi").value); // ekrandaki text içine gýrýlmýþ rakamý sayýya cevýrdým.
+
+    var karesi = sayi * sayi;
+
+    document.getElementById("sonuc").innerHTML = karesi;
+}
