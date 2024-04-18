0ad492ec4e1b0e905dbed606d91e6d9e291ecef8
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-04-18 19:12:09 +0300

    Some changes

--- P02_JS-Samples/sample04.js
+++ P02_JS-Samples/sample04.js
@@ -3,5 +3,34 @@ function toplama() {
     var sayi1 = parseInt(document.getElementById("sayi1").value);
     var sayi2 = parseInt(document.getElementById("sayi2").value);
 
+    var toplam = sayi1 + sayi2;
 
+    document.getElementById("sonuc").innerHTML = toplam;
+}
+
+function cikarma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var fark = sayi1 - sayi2;
+
+    document.getElementById("sonuc").innerHTML = fark;
+}
+
+function carpma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var carpim = sayi1 * sayi2;
+
+    document.getElementById("sonuc").innerHTML = carpim;
+}
+
+function bolme() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var bolum = sayi1 / sayi2;
+
+    document.getElementById("sonuc").innerHTML = bolum;
 }
