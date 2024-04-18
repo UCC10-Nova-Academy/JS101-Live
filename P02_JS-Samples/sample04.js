a261281603703d0019b856be52a5f1862601287f
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-04-18 19:22:14 +0300

    Some changes

--- P02_JS-Samples/sample04.js
+++ P02_JS-Samples/sample04.js
@@ -3,34 +3,34 @@ function toplama() {
     var sayi1 = parseInt(document.getElementById("sayi1").value);
     var sayi2 = parseInt(document.getElementById("sayi2").value);
 
-    var toplam = sayi1 + sayi2;
+    var sonuc = sayi1 + sayi2;
 
-    document.getElementById("sonuc").innerHTML = toplam;
+    document.getElementById("sonuc").innerHTML = sonuc;
 }
 
 function cikarma() {
     var sayi1 = parseInt(document.getElementById("sayi1").value);
     var sayi2 = parseInt(document.getElementById("sayi2").value);
 
-    var fark = sayi1 - sayi2;
+    var sonuc = sayi1 - sayi2;
 
-    document.getElementById("sonuc").innerHTML = fark;
+    document.getElementById("sonuc").innerHTML = sonuc;
 }
 
 function carpma() {
     var sayi1 = parseInt(document.getElementById("sayi1").value);
     var sayi2 = parseInt(document.getElementById("sayi2").value);
 
-    var carpim = sayi1 * sayi2;
+    var sonuc = sayi1 * sayi2;
 
-    document.getElementById("sonuc").innerHTML = carpim;
+    document.getElementById("sonuc").innerHTML = sonuc;
 }
 
 function bolme() {
     var sayi1 = parseInt(document.getElementById("sayi1").value);
     var sayi2 = parseInt(document.getElementById("sayi2").value);
 
-    var bolum = sayi1 / sayi2;
+    var sonuc = sayi1 / sayi2;
 
-    document.getElementById("sonuc").innerHTML = bolum;
+    document.getElementById("sonuc").innerHTML = sonuc;
 }
