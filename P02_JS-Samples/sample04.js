76ddef901006707665f09c2d04bb27a360216b88
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-04-18 19:05:22 +0300

    Some changes

---
+++ P02_JS-Samples/sample04.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function toplama() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+
+}
